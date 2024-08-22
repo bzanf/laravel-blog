fa823e8af8d2b11f74311d06f16f83a1bd379256
bzanf <borysfreitas@gmail.com>
2024-08-22 18:00:44 -0300

    Update post layouts

--- resources/views/post/components/post-card.blade.php
+++ resources/views/post/components/post-card.blade.php
@@ -1,27 +1,46 @@
-<div class="card">
+<div class="card h-100">
     <div class="card-body d-flex flex-column">
         <div class="d-flex justify-content-between">
-            <h4 class="mb-0">{{ $post->title }}</h4>
+            <h4 class="mb-0">
+                {{ $post->status === 0 ? '(Rascunho)' : '' }} {{ $post->title }}
+            </h4>
 
-            <div class="dropdown">
-                <button 
-                    class="btn btn-sm" 
-                    type="button" 
-                    id="dropdownMenuButton1" 
-                    data-bs-toggle="dropdown" 
-                    aria-expanded="false"
-                >
-                    <i class="bi bi-three-dots"></i>
-                </button>
-
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="#">Edit</a></li>
-                    <li><a class="dropdown-item" href="#">Delete</a></li>
-                </ul>
-            </div>
+            {{-- @if (auth()->check() && (auth()->user()->id === $post->user_id || auth()->user()->role === 'admin')) --}}
+            @can('update', $post)
+                <div class="dropdown">
+                    <button 
+                        class="btn btn-sm" 
+                        type="button" 
+                        id="dropdownMenuButton1" 
+                        data-bs-toggle="dropdown" 
+                        aria-expanded="false"
+                    >
+                        <i class="bi bi-three-dots"></i>
+                    </button>
+    
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="{{ route('post.edit', $post) }}">Alterar</a></li>
+                        <form action="{{ route('post.destroy', $post) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <li>
+                                <button 
+                                    class="dropdown-item" 
+                                    onclick="return confirm('Tem certeza que deseja excluir este post?');"
+                                >
+                                    Remover
+                                </button>
+                            </li>
+                        </form>
+                    </ul>
+                </div>
+            @endcan
+            {{-- @endif --}}
         </div>
         
-        <span class="text-muted">{{ $post->created_at }} - {{ $post->user->name }}</span>
+        <span class="text-muted">
+            {{ $post->created_at->setTimezone('America/Sao_Paulo')->format('d/m/Y H:i') }} - {{ $post->user->name }}
+        </span>
         <p 
             class="card-text mt-2 mb-0 text-ellipsis overflow-hidden"
             style="{{ !$fullContent ? 'max-height: 200px' : '' }}"
@@ -30,7 +49,7 @@
         </p>
 
         @if(!$fullContent)
-            <a class="pt-2" href="{{ route('post.show', $post) }}">Continue reading</a>
+            <a class="pt-2 mt-auto" href="{{ route('post.show', $post) }}">Continuar leitura</a>
         @endif
     </div>
 </div>
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -30,8 +30,30 @@
                 >{{ $post->content }}</textarea>
             </div>
         
-            <div class="w-100 d-flex">
-                <button type="submit" class="btn btn-primary ms-auto">Atualizar post</button>
+            <div class="w-100 d-flex align-items-center">
+                <div class="form-check form-check-inline ms-auto">
+                    <input 
+                        class="form-check-input" 
+                        type="radio" 
+                        name="status" 
+                        id="inlineRadio1" 
+                        value="0" 
+                        {{ $post->status === 0 ? 'checked' : '' }}
+                    >
+                    <label class="form-check-label" for="inlineRadio1">Rascunho</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input 
+                        class="form-check-input" 
+                        type="radio" 
+                        name="status" 
+                        id="inlineRadio2" 
+                        value="1"
+                        {{ $post->status === 1 ? 'checked' : '' }}
+                    >
+                    <label class="form-check-label" for="inlineRadio2">Publicado</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Atualizar post</button>
             </div>
         </form>
     </div>
