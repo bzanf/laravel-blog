080e795a7aaaa8c1d3e370b8234f11aec109e2f1
bzanf <borysfreitas@gmail.com>
2024-08-24 15:06:39 -0300

    Redirect home route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/post')->name('dashboard');
+Route::redirect('/home', '/post')->name('home');
 
 Auth::routes();
 
